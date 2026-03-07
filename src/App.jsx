73968ff11d9efa68bd86c5f1c8ea25ec683e2680
joseninogarcia06 <jngarcia08081988@gmail.com>
2026-03-07 13:38:26 +0800

    Add tablet support

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import '../src/Mobile/Mobile.css'
 import '../src/Mobile/LargePhones.css'
+import '../src/Mobile/Tablet.css'
 import { Route, Routes } from 'react-router-dom'
 import HomeBody from './Components/HomeBody'
 
